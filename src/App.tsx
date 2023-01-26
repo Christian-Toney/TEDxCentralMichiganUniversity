ab72b5b59701dfefcfa329ca38153b239e4458a2
Christian <christian@makuwro.com>
2023-01-26 09:29:13 -0500

    Add page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
 import "./global.css";
 
 export default function App() {
@@ -6,18 +9,22 @@ export default function App() {
   return (
     <>
       <header>
-        <a href="/">
+        <Link to="/">
           <img src="/logo.jpg" />
-        </a>
+        </Link>
         <nav>
           <ul>
             <li>
-              <a href="/about">About</a>
+              <Link to="/about">About TED</Link>
             </li>
           </ul>
         </nav>
       </header>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
       <footer>
         <p>This independent TEDx event is operated under license from TED.</p>
       </footer>
