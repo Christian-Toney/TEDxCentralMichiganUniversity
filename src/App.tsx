8756a51fbb9c09e0509feb96f72fa6c253c8edb9
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2023-02-16 22:09:21 -0500

    finish header

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,45 @@ export default function App() {
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <footer>
-        <p>This independent TEDx event is operated under license from TED.</p>
+        <section>
+          <section>
+            <h1>Explore</h1>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About TED</Link>
+              </li>
+              <li>
+                <Link to="/organizers">Organizers</Link>
+              </li>
+            </ul>
+          </section>
+          <section>
+            <h1>Community</h1>
+            <ul>
+              <li>
+                <a href="#">Tickets</a>
+              </li>
+              <li>
+                <a href="#">Livestream</a>
+              </li>
+              <li>
+                <a href="https://instagram.com/TEDxCentralMichiganUniversity" target="_blank" rel="noreferrer">Instagram</a>
+              </li>
+              <li>
+                <a href="https://github.com/Sudobeast/TEDxCentralMichiganUniversity" target="_blank" rel="noreferrer">GitHub</a>
+              </li>
+            </ul>
+          </section>
+        </section>
+        <section id="group2">
+          <p>This independent TEDx event is operated under license from TED.</p>
+          <section>
+            <button onClick={() => window.scrollTo({top: 0})}>Go to top</button>
+          </section>
+        </section>
       </footer>
     </>
   );
