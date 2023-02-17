99d14e447f78ec7f2054df365fd3b0c8beda3191
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2023-02-17 07:37:11 -0500

    update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,16 +1,81 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styles from "./Home.module.css";
 
 export default function Home() {
 
+  const [speakerComponents, setSpeakerComponents] = useState<React.ReactElement[]>([]);
+
+  useEffect(() => {
+
+    const speakers = [
+      {
+        name: "TBD",
+        title: "TBD",
+        description: "TBD",
+        link: "#"
+      },
+      {
+        name: "TBD",
+        title: "TBD",
+        description: "TBD",
+        link: "#"
+      },
+      {
+        name: "TBD",
+        title: "TBD",
+        description: "TBD",
+        link: "#"
+      },
+      {
+        name: "TBD",
+        title: "TBD",
+        description: "TBD",
+        link: "#"
+      },
+      {
+        name: "TBD",
+        title: "TBD",
+        description: "TBD",
+        link: "#"
+      },
+      {
+        name: "TBD",
+        title: "TBD",
+        description: "TBD",
+        link: "#"
+      }
+    ];
+    const speakerComponents = [];
+    for (const speaker of speakers) {
+
+      speakerComponents.push(
+        <li key={speaker.name}>
+          <a href={speaker.link}>
+            <label>{speaker.name}</label>
+            <label>{speaker.title}</label>
+            <p>{speaker.description}</p>
+          </a>
+        </li>
+      );
+
+    }
+    setSpeakerComponents(speakerComponents);
+
+  }, []);
+
   return (
     <main>
-      <section id="apply">
-        <h1>Wanna give it a go?</h1>
-        <section onClick={() => window.open("https://forms.microsoft.com/r/mmezsbdyBx")}>
-          <button>Apply to become a speaker</button>
-        </section>
-        <p>We also have <a href="/about#volunteer">volunteer opprotunities</a>.</p>
+      <section id={styles.title}>
+        <h1>The essense of a changing world</h1>
+        <p>Enter some text here.</p>
       </section>
+      <h1>Meet the students</h1>
+      <ul>
+        {speakerComponents}
+      </ul>
+      <h1>...and the pros too!</h1>
+      <h1>Featuring songs from the choir</h1>
+      <h1>We're live from French Auditorium</h1>
     </main>
   );
 
