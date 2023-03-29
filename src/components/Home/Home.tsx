b0c13944d7b7d90b902d24ef17f29d7b14b42b28
Christian <christian@makuwro.com>
2023-03-29 19:20:08 -0400

    i forgor 💀

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,37 +1,68 @@
-import React, { useEffect, useState } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
 import styles from "./Home.module.css";
 
 export default function Home() {
 
-  const [speakerComponents, setSpeakerComponents] = useState<React.ReactElement[]>([]);
+  const [studentComponents, setStudentComponents] = useState<ReactElement[]>([]);
+  const [proComponents, setProComponents] = useState<ReactElement[]>([]);
 
   useEffect(() => {
 
-    const speakers = [
+    const students = [
       {
-        name: "TBD",
-        title: "TBD",
-        description: "TBD",
+        name: "Student Name",
+        title: "Student Title",
+        description: "Student description. This'll only show when the user hovers over the card.",
         link: "#"
       },
       {
-        name: "TBD",
-        title: "TBD",
-        description: "TBD",
+        name: "Student Name",
+        title: "Student Title",
+        description: "Student description. This'll only show when the user hovers over the card.",
         link: "#"
       },
       {
-        name: "TBD",
-        title: "TBD",
-        description: "TBD",
+        name: "Student Name",
+        title: "Student Title",
+        description: "Student description. This'll only show when the user hovers over the card.",
         link: "#"
       },
       {
-        name: "TBD",
-        title: "TBD",
-        description: "TBD",
+        name: "Student Name",
+        title: "Student Title",
+        description: "Student description. This'll only show when the user hovers over the card.",
         link: "#"
       },
+      {
+        name: "Student Name",
+        title: "Student Title",
+        description: "Student description. This'll only show when the user hovers over the card.",
+        link: "#"
+      },
+      {
+        name: "Student Name",
+        title: "Student Title",
+        description: "Student description. This'll only show when the user hovers over the card.",
+        link: "#"
+      },
+    ];
+    const studentComponents = [];
+    for (const speaker of students) {
+
+      studentComponents.push(
+        <li key={speaker.name}>
+          <a href={speaker.link} className={styles.speaker}>
+            <h1 className={styles.name}>{speaker.name}</h1>
+            <h2 className={styles.title}>{speaker.title}</h2>
+            <p>{speaker.description}</p>
+          </a>
+        </li>
+      );
+
+    }
+    setStudentComponents(studentComponents);
+
+    const pros = [
       {
         name: "TBD",
         title: "TBD",
@@ -45,21 +76,21 @@ export default function Home() {
         link: "#"
       }
     ];
-    const speakerComponents = [];
-    for (const speaker of speakers) {
+    const proComponents = [];
+    for (const speaker of pros) {
 
-      speakerComponents.push(
+      proComponents.push(
         <li key={speaker.name}>
           <a href={speaker.link}>
-            <label>{speaker.name}</label>
-            <label>{speaker.title}</label>
+            <h1>{speaker.name}</h1>
+            <h2>{speaker.title}</h2>
             <p>{speaker.description}</p>
           </a>
         </li>
       );
 
     }
-    setSpeakerComponents(speakerComponents);
+    setProComponents(proComponents);
 
   }, []);
 
@@ -67,15 +98,20 @@ export default function Home() {
     <main>
       <section id={styles.title}>
         <h1>The essense of a changing world</h1>
-        <p>Enter some text here.</p>
+        <p>Enter a sub-description here.</p>
       </section>
       <h1>Meet the students</h1>
-      <ul>
-        {speakerComponents}
+      <ul className={styles.speakers}>
+        {studentComponents}
       </ul>
       <h1>...and the pros too!</h1>
+      <ul>
+        {proComponents}
+      </ul>
       <h1>Featuring songs from the choir</h1>
+      <p>[Put an image and description of the choir here. If they have a website, link to it.]</p>
       <h1>We're live from French Auditorium</h1>
+      <p>[Put the address and a button to buy tickets here.]</p>
     </main>
   );
 
