7b07e34ed7ae0c767dbcefb1e82e1b9a4b8c6a32
Christian <christian@makuwro.com>
2023-04-01 18:46:31 -0400

    add some photos

--- src/components/Organizer/Organizer.tsx
+++ src/components/Organizer/Organizer.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 
-export default function Organizer({name, title, imgSrc}: {name: string, title?: string, imgSrc?: string}) {
+export default function Organizer({name, title, imgSrc, offsetX = 0, offsetY = 0, scale = 1}: {name: string, title?: string, imgSrc?: string, offsetX?: number, offsetY?: number, scale?: number}) {
 
   return (
     <li>
-      <a href="#">
-        <section>
-          {
-            imgSrc ? (
-              <img src={imgSrc} />
-            ) : null
-          }
-        </section>
-        <b>{name}</b>
-        <p>{title ?? "Organizer"}</p>
-      </a>
+      <section>
+        {
+          imgSrc ? (
+            <img src={imgSrc} style={{transform: `scale(${scale})`, objectPosition: offsetX || offsetY ? `${offsetX}px ${offsetY}px` : undefined}} />
+          ) : null
+        }
+      </section>
+      <b>{name}</b>
+      <p>{title ?? "Organizer"}</p>
     </li>
   );
 
--- src/components/OrganizersPage/OrganizersPage.tsx
+++ src/components/OrganizersPage/OrganizersPage.tsx
@@ -7,30 +7,25 @@ export default function Organizers() {
   return (
     <main id={styles.main}>
       <section>
-        <h1>Operations Team</h1>
+        <h1>TEDxCentral Michigan University Team</h1>
         <ul>
-          <Organizer name="Tyler Thompson" />
-          <Organizer name="Mitchell Basham" />
+          <Organizer name="Tyler Thompson" imgSrc="/people/Tyler.jpg" offsetY={20} scale={1.4} />
+          <Organizer name="Mitchell Basham" imgSrc="/people/Mitchell.png" />
           <Organizer name="Natalie Brant" />
-        </ul>
-      </section>
-      <section>
-        <h1>Speaker Curation Team</h1>
-        <ul>
-          <Organizer name="Ameer Hicks" title="Head of Speaker Curation" />
-        </ul>
-      </section>
-      <section>
-        <h1>Design & Marketing Team</h1>
-        <ul>
-          <Organizer name="Ava Brewer" title="Head of Design & Marketing" />
-          <Organizer name="Christian Toney" title="Head of Technology" />
-        </ul>
-      </section>
-      <section>
-        <h1>Partnership & Sponsorship Team</h1>
-        <ul>
+          <Organizer name="Ameer Hicks" title="Head of Speaker Curation" imgSrc="/people/Ameer.jpg" offsetY={-20} />
+          <Organizer name="Ava Brewer" title="Head of Design & Marketing" imgSrc="/people/Ava.jpg" />
+          <Organizer name="Christian Toney" title="Organizer and Head of Technology" />
+          <Organizer name="Christian Mueller" title="Social Media Manager" />
+          <Organizer name="Lauren Hull" title="Marketing Staff" imgSrc="/people/Lauren.jpg" offsetY={-128} />
+          <Organizer name="Carolina Hernandez-Ruiz" title="Marketing Staff" imgSrc="/people/Carolina.jpeg" offsetY={-115} />
+          <Organizer name="Brooke Gordon" title="Marketing Staff" />
           <Organizer name="Gabby Mifsud" title="Head of Partnerships & Sponsorships" />
+        </ul>
+      </section>
+      <section>
+        <h1>Special Thanks</h1>
+        <ul>
+          <li style={{width: "auto"}}>Central Michigan University University Events</li>
         </ul>
       </section>
     </main>
