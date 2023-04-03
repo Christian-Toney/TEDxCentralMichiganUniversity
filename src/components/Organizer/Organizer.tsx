dff0ad26370e129e8a0f2df1e57128ce0cd9da08
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2023-04-03 13:56:15 -0400

    add a few more people

--- src/components/Organizer/Organizer.tsx
+++ src/components/Organizer/Organizer.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-export default function Organizer({name, title, imgSrc, offsetX = 0, offsetY = 0, scale = 1.01}: {name: string, title?: string, imgSrc?: string, offsetX?: number, offsetY?: number, scale?: number}) {
+export default function Organizer({name, title, imgSrc, offsetX, offsetY, scale = 1.01}: {name: string, title?: string, imgSrc?: string, offsetX?: number, offsetY?: number, scale?: number}) {
 
   return (
     <li>
       <section>
         {
           imgSrc ? (
-            <img src={imgSrc} style={{transform: `scale(${scale})`, objectPosition: offsetX || offsetY ? `${offsetX}px ${offsetY}px` : undefined}} />
+            <img src={imgSrc} style={{transform: `scale(${scale})`, objectPosition: offsetX !== undefined || offsetY !== undefined ? `${offsetX ?? 0}px ${offsetY ?? 0}px` : undefined}} />
           ) : null
         }
       </section>
--- src/components/OrganizersPage/OrganizersPage.tsx
+++ src/components/OrganizersPage/OrganizersPage.tsx
@@ -5,9 +5,12 @@ import tyler from "../../people/Tyler.jpg";
 import mitchell from "../../people/Mitchell.png";
 import ameer from "../../people/Ameer.jpg";
 import ava from "../../people/Ava.jpg";
-import christian from "../../people/Christian.jpg";
+import christianM from "../../people/ChristianM.jpg";
+import christianT from "../../people/ChristianT.jpg";
 import lauren from "../../people/Lauren.jpg";
 import carolina from "../../people/Carolina.jpeg";
+import brooke from "../../people/Brooke.jpg";
+import rhiannon from "../../people/Rhiannon.jpg";
 
 export default function Organizers() {
 
@@ -19,16 +22,15 @@ export default function Organizers() {
           <Organizer name="Tyler Thompson" title="Lead Organizer" imgSrc={tyler} offsetY={20} scale={1.4} />
           <Organizer name="Mitchell Basham" imgSrc={mitchell} />
           <Organizer name="Natalie Brant" />
-        </ul>
-        <ul>
           <Organizer name="Ameer Hicks" title="Head of Speaker Curation" imgSrc={ameer} offsetY={-20} />
           <Organizer name="Ava Brewer" title="Head of Design & Marketing" imgSrc={ava} />
-          <Organizer name="Christian Toney" title="Organizer and Head of Technology" imgSrc={christian} offsetY={-50} />
+          <Organizer name="Christian Toney" title="Organizer and Head of Technology" imgSrc={christianT} offsetY={-50} />
           <Organizer name="Gabby Mifsud" title="Head of Partnerships & Sponsorships" />
-          <Organizer name="Christian Mueller" title="Social Media Manager" />
+          <Organizer name="Christian Mueller" title="Social Media Manager" imgSrc={christianM} offsetY={0} />
           <Organizer name="Lauren Hull" title="Marketing Staff" imgSrc={lauren} offsetY={-128} />
           <Organizer name="Carolina Hernandez-Ruiz" title="Marketing Staff" imgSrc={carolina} offsetY={-115} />
-          <Organizer name="Brooke Gordon" title="Marketing Staff" />
+          <Organizer name="Brooke Gordon" title="Marketing Staff" imgSrc={brooke} />
+          <Organizer name="Rhiannon Grodnik" title="Marketing Staff" imgSrc={rhiannon} offsetY={-25} />
         </ul>
       </section>
       <section>
