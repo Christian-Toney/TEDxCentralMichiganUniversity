c85e21b92b5364156cdcc33dee74d320e51404c3
Christian <christian@makuwro.com>
2023-04-21 13:43:37 -0400

    finalize

--- src/components/SponsorsPage/SponsorsPage.tsx
+++ src/components/SponsorsPage/SponsorsPage.tsx
@@ -13,36 +13,36 @@ export default function SponsorsPage() {
 
   return (
     <main id={styles.main}>
-      <h1></h1>
+      <h1>Sponsors</h1>
       <ul>
         <li>
-          <img src={ignite} />
           <section>
+            <img src={ignite} />
             <h1>Ignite</h1>
-            <p>Ignite Donuts is a student run business. Located through many outlets on campus and a cafe in Mt. Pleasant, offering artisanal sweets, drinks, and homemade sandwiches. The business began from a CMU Alum who started frying donuts on campus 6 years ago. It is founded on serving and loving this community. Please visit Ignitedonuts.com and its social media outlets (@ignitedonuts) to get in touch!</p>
           </section>
+          <p>Ignite Donuts is a student run business. Located through many outlets on campus and a cafe in Mt. Pleasant, offering artisanal sweets, drinks, and homemade sandwiches. The business began from a CMU Alum who started frying donuts on campus 6 years ago. It is founded on serving and loving this community. Please visit Ignitedonuts.com and its social media outlets (@ignitedonuts) to get in touch!</p>
         </li>
         <li>
-          <img src={entrepreneurship} />
           <section>
+            <img src={entrepreneurship} />
             <h1>John Gustinic</h1>
-            <p>John Gustincic, JD, is a faculty member in the CMU Entrepreneurship Department, and serves as the Entrepreneur-in-Residence for the Isabella Bank Institute for Entrepreneurship (IBIE) at CMU.</p>
-            <p>The IBIE's goals are to teach practical skills, inspire innovative thinking, and connect students with mentors and other resources. We accomplish this through activities such as workshops, the New Venture Challenge, the Make-a-Pitch event, where students from all disciplines interact with experienced alumni, faculty, entrepreneurs, investors, and other business and political leaders to become better prepared to put their stamp on the world!</p>
           </section>
+          <p>John Gustincic, JD, is a faculty member in the CMU Entrepreneurship Department, and serves as the Entrepreneur-in-Residence for the Isabella Bank Institute for Entrepreneurship (IBIE) at CMU.</p>
+          <p>The IBIE's goals are to teach practical skills, inspire innovative thinking, and connect students with mentors and other resources. We accomplish this through activities such as workshops, the New Venture Challenge, the Make-a-Pitch event, where students from all disciplines interact with experienced alumni, faculty, entrepreneurs, investors, and other business and political leaders to become better prepared to put their stamp on the world!</p>
         </li>
         <li>
-          <img src={leadership} />
           <section>
+            <img src={leadership} />
             <h1>Dan Gaken - Leadership Institute</h1>
-            <p>Opened in 1997, the Sarah R. Opperman Leadership Institute is the nations' premier undergraduate leadership program.  The Leadership Institute serves as the coordinating body for CMU's vast student leadership development offerings.  The Institute strives to build leadership programs that are innovative, inclusive, and intentional.  The LI offers both curricular and co-curricular leadership opportunities for all students, including Leadership Safari and the dynamic Leader Advancement Scholar cohort.</p>
           </section>
+          <p>Opened in 1997, the Sarah R. Opperman Leadership Institute is the nations' premier undergraduate leadership program.  The Leadership Institute serves as the coordinating body for CMU's vast student leadership development offerings.  The Institute strives to build leadership programs that are innovative, inclusive, and intentional.  The LI offers both curricular and co-curricular leadership opportunities for all students, including Leadership Safari and the dynamic Leader Advancement Scholar cohort.</p>
         </li>
         <li>
-          <img src={bookstore} />
           <section>
+            <img src={bookstore} />
             <h1>CMU Bookstore</h1>
-            <p>Check us out at www.cmubookstore.com</p>
           </section>
+          <p>Check us out at www.cmubookstore.com</p>
         </li>
       </ul>
       <ul id={styles.noNames}>
